919a137a26493266225421c938f9540d62304628
my-romance <59412534+my-romance@users.noreply.github.com>
2020-03-05 21:28:30 +0900

    Update 체육복.py

--- programmers/체육복.py
+++ programmers/체육복.py
@@ -9,8 +9,8 @@ def solution(n, lost, reserve):
             
         # 도난 당한 학생들만 처리
         if (not(f_num == -1)) and (arr[f_num]==2) :  # 앞 학생이 빌려줄 수 있음 -> 앞 학생한테 먼저 빌린다. 뒤에 도낭 당한 학생들을 배려하기 위해. 앞학생 체육복은 안 쓰면 버려지기 때문
-            arr[f_num] -= 1 ; arr[idx] += 1; continue
-        if (not(b_num == n )) and (arr[b_num]==2) : # 뒤 학생이 빌려줄 수 있음
+            arr[f_num] -= 1 ; arr[idx] += 1;
+        elif (not(b_num == n )) and (arr[b_num]==2) : # 뒤 학생이 빌려줄 수 있음
             arr[b_num] -= 1; arr[idx] += 1;
     
     for x in arr:
